b258fc09dd39b66e3abb02ef99c1033625f184b9
Gokul N <gokul.n@experionglobal.local>
2022-12-30 14:39:23 +0530

    second commit

---
+++ 30 Dec 22 -js/obj.js
@@ -0,0 +1,17 @@
+const languages = ['malayalam', 'english', 'french', 'tamil'];
+
+languages.forEach((element, index) => console.log(index , element));
+
+// or
+// foreach(element){
+// console.log(element);   
+// }
+
+const person = {
+name: '20',
+age: '30',
+edu: 'bca'
+};
+
+console.log(person.name);
+console.log(person.age);
