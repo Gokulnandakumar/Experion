a071403a8b82a7a8835e71e366bb9ff313810802
Gokul N <gokul.n@experionglobal.local>
2023-01-04 21:46:21 +0530

    Third commit of 04 jan 2023

--- 04 Jan 23/app.js
+++ 04 Jan 23/app.js
@@ -11,14 +11,27 @@
 //   }
 
 //   longRunningTask(printf);
+const tar = document.querySelector('#target');
 
-fetch('https://reqres.in/api/users').then(res => res.json()).then((data) =>{
-    
+fetch('https://reqres.in/api/users?page=2').then(res => res.json()).then((data) => {
+
     // for (let i = 0; i < data.data.length; i++) {
     //     console.log(data.data[i].email);
     // }
 
+    // data.data.forEach(element => {
+    //     console.log(element.email);
+    // });
+
     data.data.forEach(element => {
-        console.log(element.email);
+        const row = `<tr>
+                <td>${element.id}</td>
+                <td>
+                <img src=" ${element.avatar}" height="50">
+                </td>
+                <td>${element.first_name}  ${element.last_name}</td>
+                <td>${element.email}</td>
+                </tr>`;
+                   tar.innerHTML += row;
     });
 });
