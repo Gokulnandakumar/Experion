903fdacd3c4e7ce73ffe5026a1cc3bafc70d04e5
Gokul N <gokul.n@experionglobal.local>
2023-01-04 11:36:30 +0530

    second commit of 04 jan 2023

--- 04 Jan 23/app.js
+++ 04 Jan 23/app.js
@@ -1,1 +1,24 @@
-html
+// function longRunningTask(callback) {
+
+//     setTimeout(() => {
+//       const name = 'js';
+//       callback(name);
+//     },5000);
+//   }
+
+//   function printf (res) {
+//     console.log(res);
+//   }
+
+//   longRunningTask(printf);
+
+fetch('https://reqres.in/api/users').then(res => res.json()).then((data) =>{
+    
+    // for (let i = 0; i < data.data.length; i++) {
+    //     console.log(data.data[i].email);
+    // }
+
+    data.data.forEach(element => {
+        console.log(element.email);
+    });
+});
