1381aa64157d238eee2e6f12879f402cce603337
Gokul N <gokul.n@experionglobal.local>
2023-01-24 19:49:45 +0530

    logical questions

--- c-sharp logical questions/PassengerBoardingPassQueue/Program.cs
+++ c-sharp logical questions/PassengerBoardingPassQueue/Program.cs
@@ -1,32 +1,57 @@
 ﻿
+using System.Security.Cryptography;
+
+Random random= new Random();
+
+
 Queue<int> FirstQueue = new Queue<int>();
 Queue<int> SecondQueue = new Queue<int>();
 Queue<int> ThirdQueue = new Queue<int>();
 
 // accept 10 passengers
-int[] BoardingPass = new int[10];
+int[] BoardingPass = new int[11];
 
 Console.WriteLine("enter 10 passenger boarding details");
-for (int i = 0; i < 10; i++)
+for (int i = 0; i <= 10; i++)
 {
     BoardingPass[i] = int.Parse(Console.ReadLine()); 
-    if(FirstQueue.Count <= 3)
+    if(FirstQueue.Count <3)
     {
         FirstQueue.Enqueue(BoardingPass[i]);
-        Console.WriteLine($"id {BoardingPass[i]} is inserted in position {i} in FirstQueue");
+        Console.WriteLine($"id {BoardingPass[i]} is inserted in position in FirstQueue");
     }
-    else if(SecondQueue.Count <=3)
+    else if(SecondQueue.Count <3)
     {
         SecondQueue.Enqueue(BoardingPass[i]);
-        Console.WriteLine($"id {BoardingPass[i]} is inserted in position {i} in SecondQueue");
+        Console.WriteLine($"id {BoardingPass[i]} is inserted in position in SecondQueue");
     }
-    else if(ThirdQueue.Count <=3)
+    else if(ThirdQueue.Count <3)
     {
         ThirdQueue.Enqueue(BoardingPass[i]);
-        Console.WriteLine($"id {BoardingPass[i]} is inserted in position {i} in ThirdQueue");
+        Console.WriteLine($"id {BoardingPass[i]} is inserted in position in ThirdQueue");
     }
     else
     {
-        Console.WriteLine($"space for boarding pass {BoardingPass[i]} is not available");
+        //Console.WriteLine($"space for boarding pass {BoardingPass[i]} is not available");
+
+        int val = random.Next(1,3);
+        if( val == 0 )
+        {
+            FirstQueue.Dequeue();
+            FirstQueue.Enqueue(BoardingPass[i]);
+            Console.WriteLine($"{BoardingPass[i]} is inserted in first queue");
+        }
+        else if( val == 1)
+        {
+            SecondQueue.Dequeue();
+            SecondQueue.Enqueue(BoardingPass[i]);
+            Console.WriteLine($"{BoardingPass[i]} is inserted in second queue");
+        }
+        else if( val == 2)
+        {
+            ThirdQueue.Dequeue();
+            ThirdQueue.Enqueue(BoardingPass[i]);
+            Console.WriteLine($"{BoardingPass[i]} is inserted in Third queue");
+        }
     }
 }
---
+++ c-sharp logical questions/ShortestDistanceAndFares/Program.cs
@@ -0,0 +1,120 @@
+﻿
+Console.WriteLine("enter number of values");
+int val = int.Parse(Console.ReadLine());
+object[,] arr = new object[val, 4];
+
+Console.WriteLine("enter array values");
+for (int i = 0; i < val; i++)
+{
+	for (int j = 0; j < 4; j++)
+	{
+		arr[i,j] = Console.ReadLine();
+	}
+}
+
+// Shortest Distance
+
+int ShortDistance = int.MaxValue;
+
+for (int i = 0; i < val - 1; i++)
+{
+    int j = i + 1;
+    for (int k = 2; k < 3; k++)
+    {
+        int val1 = Convert.ToInt32(arr[i, k]);
+        int val2 = Convert.ToInt32(arr[j, k]);
+        if (val1 < val2 && val1 < ShortDistance)
+        {
+            ShortDistance = val1;
+        }
+        else
+        {
+            if (val2 < val1 && val2 < ShortDistance)
+            {
+                ShortDistance = val2;
+            }
+        }
+    }
+}
+Console.WriteLine($"Shortest distance is {ShortDistance}");
+
+// Largest Distance
+int LargeDistance = 0;
+
+for (int i = 0; i < val-1; i++)
+{
+	int j = i+1;
+	for (int k = 2; k < 3; k++)
+	{
+		int val1 = Convert.ToInt32(arr[i, k]); 
+		int val2 = Convert.ToInt32(arr[j, k]);
+		if( val1 > val2 && val1 > LargeDistance)
+		{
+			LargeDistance = val1;
+		}
+		else
+		{
+			if (val2 > val1 && val2 > LargeDistance)
+			{
+				LargeDistance = val2;
+			}
+        }
+	}
+}
+Console.WriteLine($"Largest distance is {LargeDistance}");
+
+// SmallestFare
+
+int SmallFare = int.MaxValue;
+
+for (int i = 0; i < val - 1; i++)
+{
+    int j = i + 1;
+    for (int k = 3; k <= 3; k++)
+    {
+        int val1 = Convert.ToInt32(arr[i, k]);
+        int val2 = Convert.ToInt32(arr[j, k]);
+        if (val1 < val2 && val1 < SmallFare)
+        {
+            SmallFare = val1;
+        }
+        else
+        {
+            if (val2 < val1 && val2 < SmallFare)
+            {
+                SmallFare = val2;
+            }
+        }
+    }
+}
+Console.WriteLine($"Smallest Fare is {SmallFare}");
+
+// HighestFare
+
+int HighestFare = 0;
+
+for (int i = 0; i < val - 1; i++)
+{
+    int j = i + 1;
+    for (int k = 3; k <= 3; k++)
+    {
+        int val1 = Convert.ToInt32(arr[i, k]);
+        int val2 = Convert.ToInt32(arr[j, k]);
+        if (val1 > val2 && val1 > HighestFare)
+        {
+            HighestFare = val1;
+        }
+        else
+        {
+            if (val2 > val1 && val2 > HighestFare)
+            {
+                HighestFare = val2;
+            }
+        }
+    }
+}
+Console.WriteLine($"Highest Fare is {HighestFare}");
+
+// Shortest path with Smallest fare
+
+
